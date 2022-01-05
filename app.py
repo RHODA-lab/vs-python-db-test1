aabfa6ee895df22c8be5a5a214a08a552571286b
omveda <vedadashan@gmail.com>
2022-01-04 23:13:01 -0800

    app.py

--- app.py
+++ app.py
@@ -9,3 +9,7 @@ def main():
     """Test DB Connection"""
     return 'Test DB Connection!'
 
+if __name__ == '__main__':  # Script executed directly?
+    print("Hello World! Built with a Docker file.")
+    app.run(host="0.0.0.0", port=5000, debug=True,use_reloader=True)
+
