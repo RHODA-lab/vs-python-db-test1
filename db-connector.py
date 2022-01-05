e9422f7390db6001458607316176510f8d9f18d9
omveda <vedadashan@gmail.com>
2022-01-04 21:19:28 -0800

    message changes

--- db-connector.py
+++ db-connector.py
@@ -1,13 +1,13 @@
 """
-hello_flask: First Python-Flask webapp
+Test RHODA DB Access Credentials
 """
 from flask import Flask  # From module flask import class Flask
 app = Flask(__name__)    # Construct an instance of Flask class for our webapp
 
 @app.route('/')   # URL '/' to be handled by main() route handler
 def main():
-    """Say hello"""
-    return 'Hello, world!'
+    """Test DB Connection"""
+    return 'Test DB Connection!'
 
 if __name__ == '__main__':  # Script executed directly?
     print("Hello World! Built with a Docker file.")
